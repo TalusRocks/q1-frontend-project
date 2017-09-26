0a2f29403374f785a0122daa1ea4a5718723ea5d
RJ <jensenrs@gmail.com>
2017-09-26 14:16:41 -0700

    removed selected div and added hover styles

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//grab character cost
 let characterCost = document.querySelectorAll('.cost');
 
 for (let i = 0; i < characterCost.length; i++) {
@@ -7,3 +8,23 @@ for (let i = 0; i < characterCost.length; i++) {
     console.log(cost.textContent.trim())
   });
 }
+
+//show and hide sections with chevron toggle
+// let chevron = document.querySelectorAll('.fa');
+//
+// for (let i = 0; i < chevron.length; i++) {
+//   let chev = chevron[i];
+//   chev.addEventListener('click', function(event) {
+//     console.log(event.target)
+//
+//     // function toggle() {
+//     var x = document.querySelector('.filter-content');
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     // }
+// }
+//
+//   });
+// }
