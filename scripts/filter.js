d25fc30d965cfd1d97d23a0fd8ff55c6a3c30f82
RJ <jensenrs@gmail.com>
2017-10-04 12:48:57 -0700

    color filter toggles styles reversed, and bug fixed for empty heroes array

--- scripts/filter.js
+++ scripts/filter.js
@@ -15,20 +15,24 @@ var filter = {
     let heroes = state.pool.active
 
     blueToggle.addEventListener('click', function() {
-      let show = true
-
-      for (let k = 0; k < heroes.length; k++) {
-        //console.log(heroes[k].color.includes('blue'))
-        if (heroes[k].color.includes('blue')) {
-          show = true
-          break
-        } else {
-          show = false
+      let show = false
+      //if heroes is empty or greater, run this check
+      if(heroes.length > 0){
+        //if heroes has any of this color, they are showing (show = true), else, show = false
+        for (let k = 0; k < heroes.length; k++) {
+          if (heroes[k].color.includes('blue')) {
+            show = true
+            break
+          } else {
+            show = false
+          }
         }
-      } //close k loop
+      }
 
+      //if blue is showing, click hides them
+      //else, click shows them
       if (show === true) {
-        //splice blue from heroes(active), push into hidden
+        //splice blue from heroes(active), which is showing, and push into hidden, not showing
         for (let i = 0; i < heroes.length; i++) {
           if (heroes[i].color === 'blue') {
             state.hidden.push(heroes[i])
@@ -37,8 +41,7 @@ var filter = {
           }
         }
 
-        //turn toggle button to toggle-on
-        blueToggle.classList.add('blue-toggle-on')
+        blueToggle.classList.add('blue-toggle-off')
       } else {
         //splice blue from hidden, push into heroes(active)
         for (let j = 0; j < state.hidden.length; j++) {
@@ -49,12 +52,80 @@ var filter = {
           }
         }
 
-        //remove toggle-on from toggle
-        blueToggle.classList.remove('blue-toggle-on')
+        blueToggle.classList.remove('blue-toggle-off')
       }
-
+      //re-render active characters 
       render.active()
     }) //close event listener
+
+
+    redToggle.addEventListener('click', function() {
+      let show = false
+      if(heroes.length > 0){
+        for (let k = 0; k < heroes.length; k++) {
+          if (heroes[k].color.includes('red')) {
+            show = true
+            break
+          } else {
+            show = false
+          }
+        }
+      }
+      if (show === true) {
+        for (let i = 0; i < heroes.length; i++) {
+          if (heroes[i].color === 'red') {
+            state.hidden.push(heroes[i])
+            heroes.splice(i, 1)
+            i--
+          }
+        }
+        redToggle.classList.add('red-toggle-off')
+      } else {
+        for (let j = 0; j < state.hidden.length; j++) {
+          if (state.hidden[j].color === 'red') {
+            heroes.push(state.hidden[j])
+            state.hidden.splice(j, 1)
+            j--
+          }
+        }
+        redToggle.classList.remove('red-toggle-off')
+      }
+      render.active()
+    })
+
+    yellowToggle.addEventListener('click', function() {
+      let show = false
+      if(heroes.length > 0){
+        for (let k = 0; k < heroes.length; k++) {
+          if (heroes[k].color.includes('yellow')) {
+            show = true
+            break
+          } else {
+            show = false
+          }
+        }
+      }
+      if (show === true) {
+        for (let i = 0; i < heroes.length; i++) {
+          if (heroes[i].color === 'yellow') {
+            state.hidden.push(heroes[i])
+            heroes.splice(i, 1)
+            i--
+          }
+        }
+        yellowToggle.classList.add('yellow-toggle-off')
+      } else {
+        for (let j = 0; j < state.hidden.length; j++) {
+          if (state.hidden[j].color === 'yellow') {
+            heroes.push(state.hidden[j])
+            state.hidden.splice(j, 1)
+            j--
+          }
+        }
+        yellowToggle.classList.remove('yellow-toggle-off')
+      }
+      render.active()
+    })
 
   }//close colorFilter
 
