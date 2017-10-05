1fe4dfa595d7c298894ec5585248705b19916d0e
RJ <jensenrs@gmail.com>
2017-10-05 10:58:07 -0700

    old test setup

---
+++ tests/test/tests.js
@@ -0,0 +1,9 @@
+var expect = chai.expect;
+console.log(expect)
+describe('testingTest', function () {
+
+  it('testingTest is a function', function () {
+    expect(testingTest).to.be.a('function');
+  })
+
+})
