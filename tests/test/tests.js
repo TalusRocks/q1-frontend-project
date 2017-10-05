53df4c935a819e4d77b9c0e28079851e74f53410
RJ <jensenrs@gmail.com>
2017-10-05 11:14:54 -0700

    delete old tests

--- tests/test/tests.js
+++
@@ -1,9 +0,0 @@
-var expect = chai.expect;
-console.log(expect)
-describe('testingTest', function () {
-
-  it('testingTest is a function', function () {
-    expect(testingTest).to.be.a('function');
-  })
-
-})
