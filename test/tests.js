0a799cb9b27fd4174f02cb444bfcf436409877bf
RJ <jensenrs@gmail.com>
2017-10-05 11:14:13 -0700

    working tests

---
+++ test/tests.js
@@ -0,0 +1,8 @@
+let expect = chai.expect
+console.log(testingTest)
+describe('destiny js', function() {
+  it('testingTest is a function', function() {
+  //  expect(testingTest).to.be.a(function)
+    expect(1).to.equal(1)
+  })
+})
